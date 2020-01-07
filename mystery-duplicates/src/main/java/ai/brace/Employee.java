e10f80e03f7257bdc79bf0591edcf4fe1242e2f8
Qiming Chen <qimingchen91129@gmail.com>
2020-01-07 12:34:22 -0500

    fix duplicate-fix done

--- mystery-duplicates/src/main/java/ai/brace/Employee.java
+++ mystery-duplicates/src/main/java/ai/brace/Employee.java
@@ -1,17 +1,30 @@
 package ai.brace;
 
-public class Employee
-{
-    public String firstName;
-    public String middleInitial;
-    public String lastName;
-    public String socialSecurityNumber;
+public class Employee {
 
-    public Employee(String _lastName, String _firstName, String _middleInitial, String _socialSecurityNumber)
-    {
-        firstName = _firstName;
-        middleInitial = _middleInitial;
-        lastName = _lastName;
-        socialSecurityNumber = _socialSecurityNumber;
-    }
+  public String firstName;
+  public String middleInitial;
+  public String lastName;
+  public String socialSecurityNumber;
+
+  public Employee(String _lastName, String _firstName, String _middleInitial,
+      String _socialSecurityNumber) {
+    firstName = _firstName;
+    middleInitial = _middleInitial;
+    lastName = _lastName;
+    socialSecurityNumber = _socialSecurityNumber;
+  }
+
+  @Override
+  public int hashCode() {
+    return toString().hashCode();
+  }
+
+  public boolean equals(Object o) {
+    return o instanceof Employee && this.hashCode() == o.hashCode() ? true : false;
+  }
+
+  public String toString() {
+    return firstName + middleInitial + lastName + socialSecurityNumber;
+  }
 }
