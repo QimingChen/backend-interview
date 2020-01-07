883d890d9f3f057eeadcd4848abfb4a0dccaf7ac
Qiming Chen <qimingchen91129@gmail.com>
2020-01-07 10:40:38 -0500

    implement json-task1 done

--- json-manipulation/src/main/java/ai/brace/Main.java
+++ json-manipulation/src/main/java/ai/brace/Main.java
@@ -1,9 +1,25 @@
 package ai.brace;
 
+import ai.brace.entity.Book;
+import ai.brace.entity.Comment;
+import ai.brace.utils.GsonReader;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Hello, world.");
+
+
+        Book json = GsonReader.readJsonFile("a1.json", Book.class);
+        List<Comment> comments = json.getTextArray();
+        comments = comments.stream().sorted(Comparator.comparing(Comment::getId)).collect(Collectors.toList());
+
+        comments.forEach(comment -> System.out.println(comment.getTextdata()));
     }
 }
---
+++ json-manipulation/src/main/java/ai/brace/entity/Comment.java
@@ -0,0 +1,27 @@
+package ai.brace.entity;
+
+public class Comment {
+
+  private int id;
+  private String textdata;
+
+  public Comment() {
+
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTextdata() {
+    return textdata;
+  }
+
+  public void setTextdata(String textdata) {
+    this.textdata = textdata;
+  }
+}
