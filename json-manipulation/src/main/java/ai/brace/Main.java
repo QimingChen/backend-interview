bf7bc2ce230cfd1f461e6757a2ec45190dc9b681
Qiming Chen <qimingchen91129@gmail.com>
2020-01-07 10:51:36 -0500

    implement json-task2 done

--- json-manipulation/src/main/java/ai/brace/Main.java
+++ json-manipulation/src/main/java/ai/brace/Main.java
@@ -9,17 +9,30 @@ import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main
 {
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Hello, world.");
 
+        // task1
+        System.out.println("####Task1");
+        Book a1 = GsonReader.readJsonFile("a1.json", Book.class);
+        List<Comment> a1Comments = a1.getTextArray();
+        a1Comments = a1Comments.stream().sorted(Comparator.comparing(Comment::getId)).collect(Collectors.toList());
+        a1Comments.forEach(comment -> System.out.println(comment.getTextdata()));
+        System.out.println("\n\n");
 
-        Book json = GsonReader.readJsonFile("a1.json", Book.class);
-        List<Comment> comments = json.getTextArray();
-        comments = comments.stream().sorted(Comparator.comparing(Comment::getId)).collect(Collectors.toList());
-
+        // task2
+        System.out.println("####Task2");
+        Book a2 =  GsonReader.readJsonFile("a2.json", Book.class);
+        List<Comment> a2Comments = a2.getTextArray();
+        List<Comment> comments = Stream.of(a1Comments, a2Comments)
+            .flatMap(x -> x.stream())// merge
+            .sorted(Comparator.comparing(Comment::getId)) // sort
+            .collect(Collectors.toList());
         comments.forEach(comment -> System.out.println(comment.getTextdata()));
+        System.out.println("\n\n");
     }
 }
